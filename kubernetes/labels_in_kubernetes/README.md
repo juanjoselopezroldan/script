05d7d1a7ab3214770fbb96bd8318b66cd3236f0e
juan jose lopez <juanjose.lopez@guadaltech.es>
2019-12-19 12:36:13 +0100

    add new repositories and new file for to add labels to resources of kubernetes

---
+++ kubernetes/labels_in_kubernetes/README.md
@@ -0,0 +1,1 @@
+## Script for add labels to resource of kubernetes also of add this some label in the pv and pvc
---
+++ kubernetes/labels_in_kubernetes/script-labels.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function usage {
+cat <<EOF
+Usage ./script-labels -r <RESOURCE> -n <NAMESPACE> -c <CLIENT>
+Options:
+  -r <RESOURCE>  # RESOURCE (RC OR DEPLOYMENT)
+  -n <NAMESPACE> # NAMESPACE
+  -c <CLIENT>  # NAME CLIENT
+EOF
+exit 2
+}
+
+if [[ $# -eq 0 ]]; then
+	usage
+else
+	while getopts ":hr:n:hc:h:" OPTIONS; do
+		case "${OPTIONS}" in
+			r) RESOURCE=${OPTARG} ;;
+			n) NAMESPACE=${OPTARG} ;;
+			c) CLIENT=${OPTARG} ;;
+			h) usage ;;
+			*) usage ;;
+		esac
+	done
+fi
+
+
+components=$(kubectl get $RESOURCE -n $NAMESPACE | tail -n +2  | awk '{print $1}')
+pvc=$(kubectl get pvc -n $NAMESPACE | tail -n +2 | awk '{ print $1 }')
+pv=$(kubectl get pv | grep $NAMESPACE | awk '{ print $1 }')
+for c in $components ; do
+	kubectl patch $RESOURCE $c -n $NAMESPACE --patch '"spec": { "selector": { "metadata": { "matchLabels": { "client": '$CLIENT' }}}}'
+done
+for p in $pvc ; do
+	kubectl patch pvc $p -n $NAMESPACE --patch '"metadata": {"labels": { "client": '$CLIENT' }}'
+done
+for v in $pv ; do
+	kubectl patch pv $v --patch '"metadata": {"labels": { "client": '$CLIENT' }}'
+done
